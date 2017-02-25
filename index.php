4bc80dcca01736cec7b242dd552a1fc195e2495d
robslawinski <rslawinski@verizon.net>
2017-02-24 21:35:47 -0500

    Updated index.php

--- index.php
+++ index.php
@@ -423,7 +423,7 @@
         </div>
     </div>
 
-    <script src='js/jquery.min.js'></script>
+    <script src='js/jquery.min.js' type='text/javascript'></script>
 	<script src='js/bootstrap.min.js' type='text/javascript'></script>	
 	
     <!-- Contact Form JavaScript -->
