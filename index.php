89177d174101fc18a8759e1112e6fd976b1bcf09
robslawinski <rslawinski@verizon.net>
2017-02-24 22:28:08 -0500

    Updated index.php

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
     <meta name="author" content="">
     <title>Robert Slawinski's IS 219 Project 1</title>
     <!-- Bootstrap Core CSS -->
-    <link href="vendor/bootstrap/css/bootstrap.min.css"	rel="stylesheet"/>
+    <link href="css/bootstrap.min.css"	rel="stylesheet"/>
     <!-- Custom Fonts -->
     <link href="vendor/font-awesome/css/font-awesome.min.css" />
     <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css" />
